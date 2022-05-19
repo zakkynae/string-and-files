5f270ad02a017e05da2ecc68f8af2a9b65643ae5
Anastasia Soboleva <flamrosae@yandex.ru>
2022-05-19 15:16:11 +0500

    Version 1

--- StringAndFiles/Program.cs
+++ StringAndFiles/Program.cs
@@ -2,9 +2,14 @@
 var phrase = Console.ReadLine();
 Console.Write("Введите букву: ");
 var letter = char.Parse(Console.ReadLine());
-PrintPercent(phrase, letter);
+Console.WriteLine($"Процент содержания буквы {letter} в предложении - {CountPercent(phrase, letter)}%");
+Console.Write("Введите буквосочетание: ");
+var subString = Console.ReadLine();
+Console.WriteLine($"Число вхождений буквосочетания {subString} - {CountOccurrences(phrase, subString)}");
+Console.WriteLine($"Кол-во слов в предложении - {CountWords(phrase)}");
+Console.WriteLine($"Кол-во гласных в предложении - {CountVowels(phrase)}");
 
-void PrintPercent(string text, char symbol)
+double CountPercent(string text, char symbol)
 {
     var count = 0;
     var length = 0;
@@ -13,6 +18,21 @@ void PrintPercent(string text, char symbol)
         if(Char.ToLower(letter) == Char.ToLower(symbol)) count++;
         if(Char.IsLetter(letter)) length++;
     }
-    Console.WriteLine($"Процент содержания буквы {symbol} в предложении - {(double)count / (double)length}");
+    return Math.Round(100 * (double)count / length);
 }
+
+int CountOccurrences(string text, string subString) => (text.Length - text.ToLower().Replace(subString.ToLower(),
+    "").Length) / subString.Length;
+
+int CountWords(string text) => text.Split(' ').Length;
+
+int CountVowels(string text)
+{
+    var vowelLetters = new List <char> { 'а', 'и', 'е', 'ё', 'о', 'у', 'ы', 'э', 'ю', 'я'};
+    var count = 0;
+    foreach (var letter in text)
+        if(vowelLetters.Contains(Char.ToLower(letter))) count++;
+    return count;
+}
+
 
