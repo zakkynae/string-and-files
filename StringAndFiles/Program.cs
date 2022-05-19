0be73afb039f6e34acc895ba9a857ee8d9464767
Anastasia Soboleva <flamrosae@yandex.ru>
2022-05-19 12:45:34 +0500

    Print percent

---
+++ StringAndFiles/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите предложение: ");
+var phrase = Console.ReadLine();
+Console.Write("Введите букву: ");
+var letter = char.Parse(Console.ReadLine());
+PrintPercent(phrase, letter);
+
+void PrintPercent(string text, char symbol)
+{
+    var count = 0;
+    var length = 0;
+    foreach (var letter in text)
+    {
+        if(Char.ToLower(letter) == Char.ToLower(symbol)) count++;
+        if(Char.IsLetter(letter)) length++;
+    }
+    Console.WriteLine($"Процент содержания буквы {symbol} в предложении - {(double)count / (double)length}");
+}
+
