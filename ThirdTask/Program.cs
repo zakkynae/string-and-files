7e81178069f07ae8072ecbb1afec20f2dbe42ed0
Anastasia Soboleva <flamrosae@yandex.ru>
2022-05-20 10:01:18 +0500

    Task third

---
+++ ThirdTask/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите текст: ");
+var text = Console.ReadLine();
+Console.WriteLine(CheckBracket(text));
+
+string CheckBracket(string text)
+{
+    var indexesBracket = new List<int>();
+    for (int index = 0; index < text.Length; index++)
+    {
+        if (text[index] == '(')
+            indexesBracket.Add(index);
+        else if (text[index] == ')')
+        {
+            if(indexesBracket.Count > 0)
+                indexesBracket.Remove(indexesBracket.Count - 1);
+            else indexesBracket.Add(index);
+        }
+    }
+    if (indexesBracket.Count != 0) return $"Скобки расставлены неверно. Позиция первой неверной скобки {indexesBracket[0] + 1}";
+    else return "Скобки расставлены верно.";
+}
