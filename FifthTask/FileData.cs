5f760637dafcfc10f1c0a9870a7096c83e61530c
Anastasia Soboleva <flamrosae@yandex.ru>
2022-05-25 20:50:05 +0500

    Files

---
+++ FifthTask/FileData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthTask
+{
+    internal class FileData
+    {
+        public string Name;
+        public string Extension;
+        public long Length;
+        public DateTime DateOfCreation;
+
+        public FileData(string extension, long length, DateTime dateOfCreation, string name)
+        {
+            Extension = extension;
+            Length = length;
+            DateOfCreation = dateOfCreation;
+            Name = name;
+        }
+
+        public string GetString() => $"Имя файла: {Name} Разрешение файла: {Extension} Размер файла: {Length} Дата содания: {DateOfCreation}\n";
+    }
+}
---
+++ FifthTask/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection;
+namespace FifthTask
+{
+    class Program
+    {
+        private static string fileName = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\Database.txt";
+
+        static void FilingBase(string[] files)
+        {
+            foreach (var file in files)
+            {
+                try
+                {
+                    var fileInf = new FileInfo(file);
+                    var fileData = new FileData(fileInf.Extension, fileInf.Length, fileInf.CreationTime, fileInf.Name);
+                    File.AppendAllText(fileName, fileData.GetString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+            }
+        }
+        static void TraverseTree(string path)
+        {
+            var dir = new Stack<string>();//создали стек для хранения всех папок
+            if (!Directory.Exists(path)) //проверяем, существует ли данная дирректория и если существует, то =>
+            {
+                throw new ArgumentException();
+            }
+            dir.Push(path);//=> то добавляем в стек для обхода
+            while (dir.Count > 0)
+            {
+                var currentDir = dir.Pop(); // извлекаем и возвращаем текущую дирректорию их стека
+                string[] subDirs; //список поддиректорий
+                try
+                {
+                    subDirs = Directory.GetDirectories(currentDir); //получаем массив поддиректорий текущей дирректории
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                string[] files; //массив файлов текущей дирректории
+                try
+                {
+                    files = Directory.GetFiles(currentDir);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                FilingBase(files);
+                foreach (var str in subDirs)
+                    dir.Push(str);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите дирректорию, которую хотите проанализировать: ");
+            var path = Console.ReadLine();
+            TraverseTree(path);
+        }
+
+    } 
+}
+
